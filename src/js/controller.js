a90fc0db5005c6494310eb060988a7b6da763ec5
Grivik <Noxel.rp@gmail.com>
2023-05-29 19:16:31 -0400

    added recipe API

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,4 @@
-const recipeContainer = document.querySelector('.recipe');
+const recipeContainer = document.querySelector(".recipe");
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
@@ -11,3 +11,18 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886"
+    );
+    const data = await res.json();
+
+console.log(res, data);
+
+  } catch (err) {
+    alert(err);
+  }
+};
+showRecipe();
